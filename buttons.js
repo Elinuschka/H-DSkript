f312319d93633aa52e181a0973c0ac4a9fcf7668
soeren1234 <so.benjes2@gmail.com>
2019-10-24 18:58:25 +0200

    Seite mit zum Auswählen vom Fachbereich

---
+++ buttons.js
@@ -0,0 +1,19 @@
+/*
+function drawButtons() {
+    but=document.getElementById('buttons');
+    for(p=0;p<=4;p++){
+        var but1 = document.createElement("input");
+        var next = document.createElement("br");
+        but1.className= 'buttonclass';
+        but1.id='button'+p+'';
+        but1.type='button';
+        but1.value='Button '+p;
+        but1.addEventListener("click", function(){
+            alert(this.id)
+        });
+        but.appendChild(but1);
+        but.appendChild(next);
+    }
+
+}
+*/
