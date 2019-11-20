9c9907bd03ac257378cbac4637c32110d8d8418c
soeren1234 <so.benjes2@gmail.com>
2019-11-20 13:29:38 +0100

    Wilkommenseite erstellt, muss jedoch noch richtig eingebaut werden
    Drop Down Menu ertstellt jedoch das übergeben ist noch nicht möglich

---
+++ WebAppOrdner/H&D/Willkommen.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Begrüßung</title>
+    <LINK href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+
+
+<body>
+<header>
+    Herzlich Wilkommen
+</header>
+<div>
+    sobald sie bereicht sind ihren Bewrbungstest zu starten drücken sie auf START<br>
+    viel erfolg
+    <form action="Fragebogen.php">
+        <button type="submit">Start</button>
+    </form>
+
+</div>
+</body>
+</html>
